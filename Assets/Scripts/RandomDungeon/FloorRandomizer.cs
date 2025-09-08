6aca9328f6fd5034b8d42b4a94bde1d04a8c3943
Lucas <lucaspietruschka@gmx.de>
2025-09-08 20:23:43 +0200

    added Floor randomizer script for prefabs

---
+++ Assets/Scripts/RandomDungeon/FloorRandomizer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FloorRandomizer : MonoBehaviour
+{
+    // create a public array of game objects called floorPrefabs
+    public GameObject[] floorPrefabs;
+    // choose a random fllor prefab from the array to set it to active and deactivate the rest in the awake method
+    private void Awake()
+    {
+        int randomIndex = Random.Range(0, floorPrefabs.Length);
+        for (int i = 0; i < floorPrefabs.Length; i++)
+        {
+            if (i == randomIndex)
+            {
+                floorPrefabs[i].SetActive(true);
+            }
+            else
+            {
+                floorPrefabs[i].SetActive(false);
+            }
+        }
+    }
+}
